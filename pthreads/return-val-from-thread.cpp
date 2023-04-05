77ec03bc842da37a92ddaca7355a0c74c356f142
md1894 <mehul.narendra.dubey@gmail.com>
2023-04-05 16:17:32 +0530

    returning value from thread in join method

--- pthreads/return-val-from-thread.cpp
+++ pthreads/return-val-from-thread.cpp
@@ -6,9 +6,20 @@ int val = 1000;
 
 pthread_mutex_t lock;
 
-void *routine()
+void *routine(void *data){
+    int *res = (int*) malloc(sizeof(int));
+    *res = 100;
+    pthread_exit((void*)res);
+}
 
 int main(){
-
+    int *res;
+    pthread_t thread;
+    pthread_mutex_init(&lock, NULL);
+    pthread_create(&thread, NULL, routine, NULL);
+    pthread_join(thread, (void**) &res);
+    cout << "Return Value from thread is " << *res << endl;
+    pthread_mutex_destroy(&lock);
+    free(res);
     return 0;
 }
