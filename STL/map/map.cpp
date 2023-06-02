23bc6b66cc6ddf73e00aa79e946e27bc8a4af991
md1894 <mehul.narendra.dubey@gmail.com>
2023-06-02 19:21:15 +0530

    iterating over map

--- STL/map/map.cpp
+++ STL/map/map.cpp
@@ -3,6 +3,7 @@
 #include<cstring>
 #include<string>
 #include<utility>
+#include<algorithm>
 using namespace std;
 
 
@@ -10,8 +11,9 @@ int main() {
     map<string, string> cust_map;
     
     cust_map.insert(make_pair(string("0001000000257"), string("24032023000145")));
-    cust_map.insert(make_pair(string("0001000000222"), string("24032023005218")));
-    cust_map.insert(make_pair(string("0001000000458"), string("24032023045454")));
+    cust_map.insert(make_pair("0001000000222", "24032023005218"));
+    cust_map["0001000000458"] = "24032023045454";
+    cust_map [string("0001000000458")]  = string("24032023045454");
     cust_map.insert(make_pair(string("0001000000123"), string("24521278855255")));
     cust_map.insert(make_pair(string("0001000000001"), string("24055555555555")));
 
@@ -26,10 +28,33 @@ int main() {
     }else{
         cout << "customer id " + str + " Not Present " << endl;
     }
+
+    cout << "iterating over map using iterator " << endl;
+    map<string, string>::iterator it = cust_map.begin();
+    while(it != cust_map.end()){
+	    cout << "Key : " + it->first + ", Value : " + it->second << endl;
+	    it++;
+    }
+    cout << "iterating over map using for-each " << endl;
+    for(const auto &val : cust_map){
+	    cout << "cust_map["+val.first+"] = " + val.second << endl;
+    }
     return 0;
 }
 
 /*
 customer id 0001000000257 present in the map and have value 24032023000145
-customer id 0254555265652665 Not Present
+customer id 0254555265652665 Not Present 
+iterating over map using iterator 
+Key : 0001000000001, Value : 24055555555555
+Key : 0001000000123, Value : 24521278855255
+Key : 0001000000222, Value : 24032023005218
+Key : 0001000000257, Value : 24032023000145
+Key : 0001000000458, Value : 24032023045454
+iterating over map using for-each 
+cust_map[0001000000001] = 24055555555555
+cust_map[0001000000123] = 24521278855255
+cust_map[0001000000222] = 24032023005218
+cust_map[0001000000257] = 24032023000145
+cust_map[0001000000458] = 24032023045454
 */
